df4ab8ea874f6f108ff426b61b1383597b65825c
hyhy-j <lhjlhj10088@gmail.com>
2025-03-20 06:21:38 +0900

    [Feat] 회원 목록 조회 기능 구현

--- src/main/java/japbook/jpashop/domain/Address.java
+++ src/main/java/japbook/jpashop/domain/Address.java
@@ -2,7 +2,6 @@ package japbook.jpashop.domain;
 
 import jakarta.persistence.Embeddable;
 import lombok.Getter;
-import lombok.Setter;
 
 @Embeddable
 @Getter
@@ -10,15 +9,15 @@ public class Address {
 
     private String city;
     private String street;
-    private String zipconde;
+    private String zipcode;
 
     protected  Address(){
     }
 
-    public Address(String city, String street, String zipconde) {
+    public Address(String city, String street, String zipcode) {
         this.city = city;
         this.street = street;
-        this.zipconde = zipconde;
+        this.zipcode = zipcode;
     }
 
 }
