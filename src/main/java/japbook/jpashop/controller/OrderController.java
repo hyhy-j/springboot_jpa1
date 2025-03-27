80da24d758546cdba71a018d2f59ae1340699a85
hyhy-j <lhjlhj10088@gmail.com>
2025-03-27 16:37:16 +0900

    [Feat] 주문 기능 컨트롤러, html 구현

---
+++ src/main/java/japbook/jpashop/controller/OrderController.java
@@ -0,0 +1,44 @@
+package japbook.jpashop.controller;
+
+import japbook.jpashop.domain.Member;
+import japbook.jpashop.domain.item.Item;
+import japbook.jpashop.service.ItemService;
+import japbook.jpashop.service.MemberService;
+import japbook.jpashop.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    private final OrderService orderService;
+    private final MemberService memberService;
+    private final ItemService itemService;
+
+    @GetMapping("/order")
+    public String createForm(Model model) {
+        List<Member> members = memberService.findMembers();
+        List<Item> items = itemService.findItems();
+
+        model.addAttribute("members", members);
+        model.addAttribute("items", items);
+
+        return "order/orderForm";
+    }
+
+    @PostMapping
+    public String order(@RequestParam("memberId") Long memberId,
+                        @RequestParam("itemId") Long itemId,
+                        @RequestParam("count") int count) {
+        orderService.order(memberId, itemId, count); // 같은 영속성 컨텍스트 안에서 트랜잭션 처리하는 것이 좋음. 멤버 엔티티를 불러오면 컨텍스트와 관계없는 멤버 객체가 그냥 들어오는 것임.
+        return "redirect:/orders";
+    }
+
+}
